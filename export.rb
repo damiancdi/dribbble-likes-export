250a100bae1610c10c0a1ccb2b6caf611bea7693
Javier Arce <javierarce@gmail.com>
2014-07-22 10:17:56 +0200

    initial import

--- export.rb
+++ export.rb
@@ -2,25 +2,23 @@ require 'rubygems'
 require 'httparty'
 
 # Configuration
-api_key      = ENV["TUMBLR_API_KEY"]
-username     = ARGV[0] || ENV["TUMBLR_USERNAME"]
+username     = ARGV[0] || ENV["DRIBBBLE_USERNAME"]
 image_dir    = ARGV[1] || "images"
-limit        = 20  # number of posts requested each time
-download_num = 200 # number of posts to download
+limit        = 20  # number of shots requested each time
+download_num = 200 # number of shots to download
 
-class TumblrPhotoExport
+class DribbbleLikesExport
 
-  attr_accessor :username, :api_key, :image_dir, :limit, :download_num, :url
+  attr_accessor :username, :image_dir, :limit, :download_num, :url
 
-  def initialize(username, api_key, image_dir, limit, download_num)
+  def initialize(username, image_dir, limit, download_num)
 
     @username     = username
-    @api_key      = api_key
     @image_dir    = image_dir
     @limit        = limit
     @download_num = download_num
 
-    @url          = "http://api.tumblr.com/v2/blog/#{@username}.tumblr.com/likes?api_key=#{@api_key}"
+    @url          = "http://api.dribbble.com/players/#{@username}/shots/likes"
 
     create_download_dir
 
@@ -34,28 +32,19 @@ class TumblrPhotoExport
 
   def get_liked_count
 
-    response        = HTTParty.get(@url + "&limit=1&offset=0")
+    response        = HTTParty.get(@url + "&per_page=1&page=0")
     parsed_response = JSON.parse(response.body)
 
-    return parsed_response['response']['liked_count']
+    return parsed_response['total']
 
   end
 
   def get_photos(limit = 0, offset = 0)
 
-    response        = HTTParty.get(@url + "&limit=#{limit}&offset=#{offset}")
+    response        = HTTParty.get(@url + "?per_page=#{limit}&page=#{offset}")
     parsed_response = JSON.parse(response.body)
 
-    # Status of the request
-    status_code = parsed_response['meta']['status']
-    status_msg  = parsed_response['meta']['msg']
-
-    if status_code != 200
-      puts "\033[91m#{status_msg}\033[0m" 
-      return
-    end
-
-    download_likes(parsed_response['response']['liked_posts'])
+    download_likes(parsed_response['shots'])
 
     return true
 
@@ -65,27 +54,22 @@ class TumblrPhotoExport
 
     likes.each do |like|
 
-      photos = like['photos']
+      puts "\033[37m#{like['title']}\033[0m" 
 
-      puts "\033[37m#{like['blog_name']}\033[0m" if photos and photos.length > 0
+      begin
 
-      photos.each do |photo|
+        uri = like['image_url']
+        file = File.basename(uri)
 
-        begin
-
-          uri = photo['original_size']['url']
-          file = File.basename(uri)
-
-          File.open("./#{@image_dir}/" + file, "wb") do |f| 
-            puts "   #{uri}"
-            f.write HTTParty.get(uri).parsed_response
-          end
-
-        rescue => e
-          puts ":( #{e}"
+        File.open("./#{@image_dir}/" + file, "wb") do |f| 
+          puts "   #{uri}"
+          f.write HTTParty.get(uri).parsed_response
         end
 
-      end if photos
+      rescue => e
+        puts ":( #{e}"
+      end
+
     end
 
   end
@@ -96,10 +80,13 @@ class TumblrPhotoExport
     # download_num = get_liked_count
 
     parsed = 0
+
     rest = @download_num % @limit
+
     if rest > 1
       rest = 1
     end
+
     batchs = (@download_num / @limit) + rest
 
     if (@download_num < @limit)
@@ -107,7 +94,7 @@ class TumblrPhotoExport
       @limit  = @download_num
     end
 
-    puts "Downloading \033[32m#{@download_num}\033[0m posts"
+    puts "Downloading \033[32m#{@download_num}\033[0m shots"
 
     batchs.times do |i|
 
@@ -116,7 +103,7 @@ class TumblrPhotoExport
       if parsed + @limit > @download_num
         @limit = @download_num - parsed
       end
-      
+
       result = get_photos(@limit, offset)
       parsed += @limit
       break if !result
@@ -129,5 +116,5 @@ class TumblrPhotoExport
 
 end
 
-tumblr = TumblrPhotoExport.new(username, api_key, image_dir, limit, download_num)
-tumblr.download
+dribbble = DribbbleLikesExport.new(username, image_dir, limit, download_num)
+dribbble.download
